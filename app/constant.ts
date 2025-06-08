4ad70e6af27d27b50f94d3e1dbbb0645f2a52325
KevinShiCN <35432622+KevinShiCN@users.noreply.github.com>
2025-06-08 23:53:42 +0800

    Added gemini-2.5-pro-preview-06-05

--- app/constant.ts
+++ app/constant.ts
@@ -545,6 +545,7 @@ const googleModels = [
   "gemini-2.0-flash-thinking-exp-1219",
   "gemini-2.0-flash-thinking-exp-01-21",
   "gemini-2.0-pro-exp",
+  "gemini-2.5-pro-preview-06-05",
   "gemini-2.0-pro-exp-02-05",
 ];
 
