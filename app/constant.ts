832104e95b99b6d093194549884398a7e2f5de7d
KevinShiCN <szsyshijiawen@gmail.com>
2025-07-12 11:24:48 +0800

    去掉gemini-2.5-flash-preview-05-20的Preview后缀

--- app/constant.ts
+++ app/constant.ts
@@ -556,7 +556,7 @@ const googleModels = [
   "gemini-2.0-pro-exp-02-05",
   "gemini-2.5-pro-preview-06-05",
   "gemini-2.5-pro",
-  "gemini-2.5-flash-preview-05-20",
+  "gemini-2.5-flash",
 ];
 
 const anthropicModels = [
